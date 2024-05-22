701b44de4a29167588d80f6dbce45b34ebfa998d
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-05-22 13:50:32 +0200

    Setup basic game controller

---
+++ api-rest/app/Models/Game.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'dice_1',
+        'dice_2',
+    ];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
---
+++ api-rest/database/migrations/2024_05_22_084858_create_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained(
+                    table: 'users',
+                    indexName: 'fk_games_users',
+                );
+            $table->unsignedInteger('dice_1');
+            $table->unsignedInteger('dice_2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
