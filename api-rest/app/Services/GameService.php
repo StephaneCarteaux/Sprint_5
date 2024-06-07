e5af5f6e7c9e11fda17ec71071b6f0928539cbb3
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-06-07 11:58:00 +0200

    Update GameService

--- api-rest/app/Services/GameService.php
+++ api-rest/app/Services/GameService.php
@@ -3,19 +3,22 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Models\Game;
 use Spatie\Permission\Models\Role;
 
 
 class GameService
 {
-
-    public function getPlayers(){
+    // Get players
+    public function getPlayers()
+    {
 
         $playerRole = Role::where('name', 'player')->first();
         $players = $playerRole->users()->get();
         return $players;
     }
 
+    // Get players with stats
     public function getPlayersWithStats()
     {
         // Add games_won_percentage to each player
@@ -29,6 +32,66 @@ class GameService
         return $playersWithStats;
     }
 
+    // Get ranking
+    public function getRanking($playersWithStats)
+    {
+        // Sort players by games_won_percentage
+        $ranking = $playersWithStats->sortBy([
+            ['games_won_percentage', 'desc'],
+            ['created_at', 'asc'],
+        ])->select('nickname', 'games_won_percentage')->values();
+        return $ranking;
+    }
+
+    // Get loser
+    public function getLoser($playersWithStats)
+    {
+        $loser = $playersWithStats->sortBy([
+            ['games_won_percentage', 'asc'],
+            ['created_at', 'asc'],
+        ])->select('nickname', 'games_won_percentage')->values()->first();
+
+        return $loser;
+    }
+
+    // Get winner
+    public function getWinner($playersWithStats)
+    {
+        $winner = $playersWithStats->sortBy([
+            ['games_won_percentage', 'd'],
+            ['created_at', 'asc'],
+        ])->select('nickname', 'games_won_percentage')->values()->first();
+        return $winner;
+    }
+
+    // Play
+    public function play($player)
+    {
+        $dice_1 = rand(1, 6);
+        $dice_2 = rand(1, 6);
+
+        $data = [
+            'player_id' => $player->id,
+            'dice_1' => $dice_1,
+            'dice_2' => $dice_2
+        ];
+
+        return $data;
+    }
+
+    // Get games
+    public function getGamesWithResult(User $player)
+    {
+        $games = Game::where('user_id', $player->id)->select('dice_1', 'dice_2')->get();
+        foreach ($games as $game) {
+            $sum = $game->dice_1 + $game->dice_2;
+            $game->result = $sum === 7 ? 'won' : 'lost';
+        }
+
+        return $games;
+    }
+
+    // Get percentage of games won by user
     public function getPercentageOfGamesWonByUser(User $player)
     {
         $totalGames = $player->games()->count();
@@ -39,6 +102,7 @@ class GameService
         return $wonGames / $totalGames * 100;
     }
 
+    // Get average percentage of games won
     public function getAveragePercentageOfGamesWon()
     {
         return round($this->getPlayersWithStats()->avg('games_won_percentage'), 2);
